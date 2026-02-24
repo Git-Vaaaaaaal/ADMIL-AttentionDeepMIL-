dde32e61c6fd167bc7e25002f08b39666f033574
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-24 15:52:53 +0100

    update script to execute main.py instead of train.py

--- train.sh
+++ train.sh
@@ -10,4 +10,4 @@ module load python
 export PYTHONPATH=/work/imvia/in156281/ADMIL-AttentionDeepMIL-/venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/imvia/in156281/.cache/matplotlib
 
-python /beegfs/data/work/imvia/in156281/ADMIL-AttentionDeepMIL-/train.py
+python /beegfs/data/work/imvia/in156281/ADMIL-AttentionDeepMIL-/main.py
